d2eaf16302f8c63c54eb2cf0dba2a7bae284b29f
Pratish Bhandari <pratishbhandari2@gmail.com>
2024-04-17 20:54:45 +0545

    Fix some user route error

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { createUser, loginUser, logoutUser } from '../controllers/user.controller.js'
+import { createUser, loginUser, logoutUser, updateUser } from '../controllers/user.controller.js'
 import { authMiddleware } from '../middleware/authUserMiddleware.js'
 
 const router = express.Router()
@@ -11,6 +11,7 @@ router.post('/login', loginUser)
 
 router.use(authMiddleware)
 router.get("/logout", logoutUser)
+router.get("/update", updateUser)
 
 
 
